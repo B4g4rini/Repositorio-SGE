2f2b8e6f7bc664e72d0baef632fdb33b262a3b53
DARK GHOST <pedro.bagarini2000@gmail.com>
2024-02-29 08:07:07 -0300

    Alteração no appsettings

--- SGE/Models/Aluno.cs
+++ SGE/Models/Aluno.cs
@@ -6,15 +6,17 @@ namespace SGE.Models
     public class Aluno
     {
         public Guid AlunoId { get; set; }
+
         [Required(ErrorMessage = "O campo Matrícula é obrigatório")]
         [Display(Name = "Matrícula")]
         public string Matricula { get; set; }
 
         [Required(ErrorMessage = "O campo Nome é obrigatório")]
-        [MinLength(3, ErrorMessage = "O campo Nome deve ter no " + "máximo 3 caracteres")]
-        [StringLength(100, ErrorMessage = "O campo Nome deve ter no " + "máximo 100 caracteres")]
+        [MinLength(3, ErrorMessage = "O campo Nome deve ter no " +
+                       "mínimo 3 caracteres")]
+        [StringLength(100, ErrorMessage = "O campo Nome deve ter no " +
+            "máximo 100 caracteres")]
         [Display(Name = "Nome do Aluno(a)")]
-
         public string AlunoNome { get; set; }
 
         [Required(ErrorMessage = "O campo Email é obrigatório")]
@@ -32,12 +34,13 @@ namespace SGE.Models
 
         [DataType(DataType.Date)]
         [Display(Name = "Data de Nascimento")]
-        [Required(ErrorMessage = "O campo Data de Nascimentop é obrigatório")]
+        [Required(ErrorMessage = "O campo Data de Nascimento é obrigatório")]
         public DateTime DataNascimento { get; set; }
+
         public bool CadAtivo { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime? CadInativo { get; set; }
-        public Guid TipoUsuarioId { get; set; }
+        public Guid? TipoUsuarioId { get; set; }
         public TipoUsuario? TipoUsuario { get; set; }
         public string? UrlFoto { get; set; }
         public ICollection<AlunoTurma>? AlunoTurmas { get; set; }
